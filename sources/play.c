e58fbd35c126e6e399a6d52f47f572a342f97cc8
Miguel Ferreira <miguelferreira1996@gmail.com>
2021-06-13 19:11:01 +0100

    Alterações finais

--- sources/play.c
+++ sources/play.c
@@ -93,12 +93,13 @@ void showPreviousTurns(gameInfo *info) {
     int turns;
 
     printf("Introduza quantas jogadas pretende rever:\n");
-    turns = checkInt(1, info->turn-1);
+    turns = checkInt(1, info->turn);
 
     for (int i = 0; i < turns; ++i) {
         infoAux = infoAux->nextTurns;
 
         if (infoAux == NULL) {
+            printf("infoAux esta null!\n");
             break;
         }
 
